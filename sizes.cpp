b1aed97626dd304f00473bc33a704cdef4cebf31
Rabia-Anwar <rabicheema250@gmail.com>
2024-10-06 19:11:04 +0000

    task 1

---
+++ sizes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+int a;
+long b;
+double c;
+float d;
+char e;
+cout<<"size of int"<<sizeof(a)<<endl;
+cout<<"size of long"<<sizeof(b)<<endl;
+cout<<"size of double"<<sizeof(c)<<endl;
+cout<<"size of float"<<sizeof(d)<<endl;
+cout<<"size of char"<<sizeof(e)<<endl;
+return 0;
+}
